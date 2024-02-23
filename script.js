c7df5a427d978f244a7c32313e2569ba9d49614b
sanjar003 <148349725+sanjar003@users.noreply.github.com>
2024-02-23 21:15:50 +0600

    todoList_JS

---
+++ script.js
@@ -0,0 +1,35 @@
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+input.addEventListener('keyup', () => {
+  if (input.value.trim() !== "0") {
+    addBtn.classList.add('active');
+  } else {
+    addBtn.classList.remove('active');
+  }
+});
+
+addBtn.addEventListener('click', () => {
+  if (input.value.trim() !== "0") {
+    let newItem = document.createElement('div');
+    newItem.classList.add('item');
+    newItem.innerHTML = `
+      <p>${input.value}</p>
+      <div class="item-btn">
+        <i class="fa-solid fa-xmark">delete</i>
+      </div>
+    `;
+    tasks.appendChild(newItem);
+    input.value = '';
+  } else {
+    alert('Please enter a task');
+  }
+});
+
+tasks.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-xmark')) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
+
